1ad23f394271d96b32551547919ccd67663d3ef5
loctt12345 <78459809+loctt12345@users.noreply.github.com>
2022-06-09 13:11:14 +0700

    new attention

--- Algorithm/model/encoder/lstm_encoder.py
+++ Algorithm/model/encoder/lstm_encoder.py
@@ -24,7 +24,7 @@ from config.lstm_encoder_cfg import lstm_encoder_CFG
 from memory_profiler import memory_usage
 
 class FeatureExtractor(nn.Module):
-    """ Used for extrating features for states/actions/rewards """
+    """ Used for extrating features for states/actions/rewards"""
 
     def __init__(self, input_size, output_size, activation_function, device):
         super(FeatureExtractor, self).__init__()
@@ -42,13 +42,88 @@ class FeatureExtractor(nn.Module):
         else:
             return torch.zeros(0, ).to(self.device)
 
+class ScaledDotProductAttention(nn.Module):
+    ''' Scaled Dot-Product Attention '''
+
+    def __init__(self, temperature, attn_dropout=0.1):
+        super().__init__()
+        self.temperature = temperature
+        self.dropout = nn.Dropout(attn_dropout)
+
+    def forward(self, q, k, v, mask=None):
+
+        attn = torch.matmul(q / self.temperature, k.transpose(2, 3))
+
+        if mask is not None:
+            attn = attn.masked_fill(mask == 0, -1e9)
+
+        attn = self.dropout(F.softmax(attn, dim=-1))
+        output = torch.matmul(attn, v)
+
+        return output, attn
+
+
+class MultiHeadAttention(nn.Module):
+    ''' Multi-Head Attention module '''
+
+    def __init__(self, n_head, d_model, d_k, d_v, dropout=0.1):
+        super().__init__()
+
+        self.n_head = n_head
+        self.d_k = d_k
+        self.d_v = d_v
+
+        self.w_qs = nn.Linear(d_model, n_head * d_k, bias=False)
+        self.w_ks = nn.Linear(d_model, n_head * d_k, bias=False)
+        self.w_vs = nn.Linear(d_model, n_head * d_v, bias=False)
+        self.fc = nn.Linear(n_head * d_v, d_model, bias=False)
+
+        self.attention = ScaledDotProductAttention(temperature=d_k ** 0.5)
+
+        self.dropout = nn.Dropout(dropout)
+        self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
+
+
+    def forward(self, q, k, v, mask=None):
+        q = q.transpose(0, 1)
+        k = k.transpose(0, 1)
+        v = v.transpose(0, 1)
+
+        d_k, d_v, n_head = self.d_k, self.d_v, self.n_head
+        sz_b, len_q, len_k, len_v = q.size(0), q.size(1), k.size(1), v.size(1)
+
+        residual = q
+        # Pass through the pre-attention projection: b x lq x (n*dv)
+        # Separate different heads: b x lq x n x dv
+        q = self.w_qs(q).view(sz_b, len_q, n_head, d_k)
+        k = self.w_ks(k).view(sz_b, len_k, n_head, d_k)
+        v = self.w_vs(v).view(sz_b, len_v, n_head, d_v)
+
+        # Transpose for attention dot product: b x n x lq x dv
+        q, k, v = q.transpose(1, 2), k.transpose(1, 2), v.transpose(1, 2)
+
+        if mask is not None:
+            mask = mask.unsqueeze(1)   # For head axis broadcasting.
+
+        q, attn = self.attention(q, k, v, mask=mask)
+
+        # Transpose to move the head dimension back: b x lq x n x dv
+        # Combine the last two dimensions to concatenate all the heads together: b x lq x (n*dv)
+        q = q.transpose(1, 2).contiguous().view(sz_b, len_q, -1)
+        q = self.dropout(self.fc(q))
+        q += residual
+
+        q = self.layer_norm(q)
+        
+        return q.transpose(0, 1), attn
+
 class Attention(nn.Module):
 
     def __init__(self, CFG, input_dim) -> None:
         super(Attention, self).__init__()
         self.CFG = CFG
-        self.attn_1 = nn.MultiheadAttention(self.CFG.lstm_hidden_dim, 1, kdim=self.CFG.lstm_hidden_dim, vdim=self.CFG.lstm_hidden_dim, dropout=0.8)
-        self.attn_2 = nn.MultiheadAttention(self.CFG.lstm_hidden_dim, 1, kdim=self.CFG.lstm_hidden_dim, vdim=self.CFG.lstm_hidden_dim, dropout=0.8)
+        self.attn_1 = MultiHeadAttention(1, self.CFG.lstm_hidden_dim, d_k=self.CFG.lstm_hidden_dim, d_v=self.CFG.lstm_hidden_dim, dropout=0.8)
+        self.attn_2 = MultiHeadAttention(1, self.CFG.lstm_hidden_dim, d_k=self.CFG.lstm_hidden_dim, d_v=self.CFG.lstm_hidden_dim, dropout=0.8)
         self.linear = nn.Linear(input_dim, self.CFG.lstm_hidden_dim)
 
     def forward(self, input, list_hiddens):
@@ -67,15 +142,20 @@ class Attention(nn.Module):
         output_1, _ = self.attn_2(input, hiddens_1, hiddens_1)
         output_0 = output_0.view(self.CFG.lstm_layers, output_0.shape[1] // 2, -1)
         output_1 = output_1.view(self.CFG.lstm_layers, output_1.shape[1] // 2, -1)
-        """
+        
         if (output_0.isnan().any() or output_1.isnan().any()):
-            print("Hidden_0: ", hiddens_0)
-            print("Hidden_1: ", hiddens_1)
-            print("Output_1: ", output_1)
-            print("Output_0: ", output_0)
-            print("Input: ", input)
-            print("List hidden: ", list_hiddens)
-        """
+            f = open("/home/loc/Desktop/ML/Project/Meta-Model-Based-RL/Algorithm/sources/hiddens", "a")
+            f.write("-----------------------------Output_0----------------------------")
+            f.write(str(output_0))
+            f.write("----------------------------Output_1-----------------------------")
+            f.write(str(output_1))
+            f.write("---------------------------attn_1---------------------------------")
+            for params in self.attn_1.parameters():
+                f.write(str(params))
+            f.write("---------------------------attn_2---------------------------------")
+            for params in self.attn_2.parameters():
+                f.write(str(params))
+        
         return (output_0, output_1)
 
 
@@ -101,6 +181,7 @@ class lstm_encoder(nn.Module):
         self.lstm = nn.LSTM(self.input_dim, self.CFG.lstm_hidden_dim, self.CFG.lstm_layers)
         self.fc_mu = nn.Linear(self.CFG.lstm_hidden_dim, self.CFG.latent_dim)
         self.fc_logvar = nn.Linear(self.CFG.lstm_hidden_dim, self.CFG.latent_dim)
+
         self.attn = Attention(CFG, self.input_dim)
         self.list_saved_hidden = deque([], maxlen=self.CFG.n_saved_hidden)
     
@@ -125,14 +206,18 @@ class lstm_encoder(nn.Module):
         if (self.CFG.use_reward):
             reward = self.fc_reward(reward)
             input = torch.cat((input, reward), dim = 1)
+
         input = input.reshape(-1,input.shape[0],input.shape[1])
         #input shape = (1, 256, 30)
-    
-        if len(self.list_saved_hidden) >= self.CFG.n_saved_hidden:
-            old_hidden_1 = self.attn.forward(input, self.list_saved_hidden)
-            old_hidden = old_hidden_1
+
+        old_hidden = self.attn.forward(input, self.list_saved_hidden)
 
         out, hidden = self.lstm(input, old_hidden)
+
+        #f = open("/home/loc/Desktop/ML/Project/Meta-Model-Based-RL/Algorithm/sources/hiddens", "a")
+        #f.write(str(old_hidden[1][1]) + "\n")
+        #for params in self.lstm.parameters():
+        #   f.write(str(params))
 
         self.list_saved_hidden.append(hidden)
         out = out.reshape(-1, self.CFG.lstm_hidden_dim)
@@ -147,7 +232,7 @@ def main():
     print(logger_kwargs)
     CFG.print()
 
-    encoder = lstm_encoder(CFG) 
+    encoder = lstm_encoder(CFG)
     print(encoder)
     hidden = (torch.zeros([1, CFG.batch_size, CFG.lstm_hidden_dim], dtype=torch.float), torch.zeros([1, CFG.batch_size, CFG.lstm_hidden_dim], dtype=torch.float))
     for i in range(10):
